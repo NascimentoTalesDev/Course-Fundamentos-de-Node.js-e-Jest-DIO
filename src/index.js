5a0eac614851d322a71da293854350654d41bcb0
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-01-31 21:58:53 -0300

    feat: add index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("Hello world!");
+import express from "express"
+
+const PORT = 5000
+const service = express()
+
+service.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+})
